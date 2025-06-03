39df09811ce8ab36e1d96d55ff56ccb679d70a8b
Vivek Kumar <vh118854@gmail.com>
2025-06-03 21:41:02 +0530

    Added login feature

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Cohort_By_Harkirat
-Week 5.1 :- HTTP Deep Dive 
+# Cohort_By_Harkirat All Notes 
+Week 5 :- HTTP Deep Dive Middleware and more
 Link:- https://petal-estimate-4e9.notion.site/Fetch-API-2a5757a63dc247289fccb1fa8fc63600
+
+week 6 :- Authentication
+Link:- https://petal-estimate-4e9.notion.site/Authentincation-a4b43c7cc1d14535a7b5b366080095fa
---
+++ week-06/index.js
@@ -0,0 +1,79 @@
+import express from 'express'
+
+const app = express();
+app.use(express.json());
+
+//Abhi database ka use nhi kar rhe hai isiliye array me store kara rhe hai
+const users = [];
+
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+app.post("/signUp", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        msg: "You Successfully SignedUp"
+    })
+})
+
+app.post("/signIn", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+    for(let i=0; i<users.length; i++) {
+        if(users[i].username == username && users[i].password == password) {
+            foundUser = users[i];
+        }
+    }
+
+    if(foundUser) {
+        const token = generateToken();
+        foundUser.token = token;
+        res.json({
+            token: token
+        })
+    } else {
+        res.status(403).json({
+            message: "Invalid Username and Password"
+        })
+    }
+})
+
+app.get("/me", (req, res) => {
+    const token = req.headers.token;
+    
+    let foundUser = null;
+    for(let i=0; i<users.length; i++) {
+        if(users[i].token == token) {
+            foundUser = users[i];
+        }
+    }
+
+    if(foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    } else {
+        res.status(401).json({
+            message : "Invalid Token"
+        })
+    }
+})
+app.listen(3000);
