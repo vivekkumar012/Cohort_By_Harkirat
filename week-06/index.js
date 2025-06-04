dad7e313dab4b422e1948dce855e5d9e222270a8
Vivek Kumar <vh118854@gmail.com>
2025-06-04 08:10:51 +0530

    Added login feature

--- week-06/index.js
+++ week-06/index.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = "vivekkalaptop"
 
 const app = express();
 app.use(express.json());
@@ -6,16 +9,16 @@ app.use(express.json());
 //Abhi database ka use nhi kar rhe hai isiliye array me store kara rhe hai
 const users = [];
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
 
 app.post("/signUp", function(req, res) {
     const username = req.body.username;
@@ -43,8 +46,13 @@ app.post("/signIn", function(req, res) {
     }
 
     if(foundUser) {
-        const token = generateToken();
-        foundUser.token = token;
+        // const token = generateToken();  // Ye to hm generate kr rhe the ek func k through ab jwt se create karenge
+
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET);
+
+        // foundUser.token = token;
         res.json({
             token: token
         })
@@ -57,10 +65,12 @@ app.post("/signIn", function(req, res) {
 
 app.get("/me", (req, res) => {
     const token = req.headers.token;
+    const decodedInformation = jwt.verify(token, JWT_SECRET);
+    const username = decodedInformation.username;
     
     let foundUser = null;
     for(let i=0; i<users.length; i++) {
-        if(users[i].token == token) {
+        if(users[i].username == username) {
             foundUser = users[i];
         }
     }
