efde4dc6231bb838b158b7b0600ce8cb95f49572
Vivek Kumar <vh118854@gmail.com>
2025-06-15 14:30:11 +0530

    react week

--- week-09/src/App.jsx
+++ week-09/src/App.jsx
@@ -1,10 +1,42 @@
-
+import React, { useState } from 'react';
 import './App.css'
 
 function App() {
   return (
-    <div>Hi there !!!</div>
+    <div style={{height: "100vh"}}>
+      <ToggleMessage />
+    </div>
   )
 }
+
+// const ToggleMessage = () => {
+//     const [isVisible, setIsVisible] = useState(false);
+
+//     return (
+//         <div>
+//             <button onClick={() => setIsVisible(!isVisible)}>
+//                 Toggle Message
+//             </button>
+//             {isVisible && <p>This message is conditionally rendered!</p>}
+//         </div>
+//     );
+// };
+
+const ToggleMessage = () => {
+    const [notificationCount, setNotificationCount] = useState(false);
+
+    function increment(){
+      setNotificationCount(notificationCount + 1);
+    }
+    return (
+        <div>
+            <button onClick={increment}>
+                Toggle Message
+            </button>
+            {notificationCount}
+        </div>
+    );
+};
+
 
 export default App
