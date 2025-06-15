c36258cdaafcfee1bbf117cff7f0d440cfc336c2
Vivek Kumar <vh118854@gmail.com>
2025-06-15 15:38:44 +0530

    react week

--- week-09/src/App.jsx
+++ week-09/src/App.jsx
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
 import './App.css'
+import { PostComponent } from './Post';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const PostComponents = posts.map(post => <PostComponent
+    name={post.name}
+    subtitle={post.subtitle}
+    time={post.time}
+    image={post.image}
+    description={post.description}
+    />
+  )
+
+  function addPost() {
+    setPosts([...posts, {
+      name: "Vivek",
+      subtitle: "10000 followers",
+      time: "2m ago",
+      image: "https://imgs.search.brave.com/UrFiXdk4j3jnRsZHUcHTZN8Lp3qLQdHIgruTAaBSX2s/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzExLzIwLzM1LzU1/LzM2MF9GXzExMjAz/NTU1NzBfSXRXMkli/NVFQM1UzemNVVnRU/WlRicE1NS3lCdVdp/RGIuanBn",
+      description: "Hi all i am here to learn react for my development journey"
+    }])
+  }
+
   return (
-    <div style={{height: "100vh"}}>
-      <ToggleMessage />
+    <div style={{height: "100vh", background: "pink"}}>
+      <button onClick={addPost}>Add Post</button>
+      <div style={{display: "flex-col", justifyContent: "center"}}>
+        {PostComponents}
+      </div>
     </div>
   )
 }
@@ -22,21 +47,21 @@ function App() {
 //     );
 // };
 
-const ToggleMessage = () => {
-    const [notificationCount, setNotificationCount] = useState(false);
+// const ToggleMessage = () => {
+//     const [notificationCount, setNotificationCount] = useState(false);
 
-    function increment(){
-      setNotificationCount(notificationCount + 1);
-    }
-    return (
-        <div>
-            <button onClick={increment}>
-                Toggle Message
-            </button>
-            {notificationCount}
-        </div>
-    );
-};
+//     function increment(){
+//       setNotificationCount(notificationCount + 1);
+//     }
+//     return (
+//         <div>
+//             <button onClick={increment}>
+//                 Toggle Message
+//             </button>
+//             {notificationCount}
+//         </div>
+//     );
+// };
 
 
 export default App
