3941c64e77158ce239e888aa532cc4ad2e75ba51
Vivek Kumar <vh118854@gmail.com>
2025-06-16 22:46:19 +0530

    last updated

--- week-10/part-01/src/App.jsx
+++ week-10/part-01/src/App.jsx
@@ -1,10 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
+      
+    
     <BrowserRouter>
+      <Link to='/'>ALLEN</Link>
+      <Link to='/neet/class-11'>Class-11</Link>
       <Routes>
         <Route path='/neet/class-11' element={<Class11Program />} />
         <Route path='/' element={<Index />}/>
@@ -21,9 +25,15 @@ function Index() {
   )
 }
 function Class11Program() {
+  const navigate = useNavigate();
+
+  function handle() {
+    navigate("/")
+  }
   return (
     <div>
       This is class 11th program
+      <button onClick={handle}>Click to go home page</button>
     </div>
   )
 }
