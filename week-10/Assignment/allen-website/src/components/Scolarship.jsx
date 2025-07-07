45cc8780e74a06208c9afcfc46eeae639d50e9fc
Vivek Kumar <vh118854@gmail.com>
2025-07-07 22:57:52 +0530

    last

--- week-10/Assignment/allen-website/src/components/Scolarship.jsx
+++ week-10/Assignment/allen-website/src/components/Scolarship.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Scolarship() {
   return (
     <div>
-      This belongs to Scolardhip pages 
+      <div className='bg-gray-500 w-[300px] h-screen flex-col space-y-4'>
+        <div>
+          <Link className='text-xl text-blue-950 font-bold hover:text-black'>Bihar Scolorship</Link>
+        </div>
+        <div>
+          <Link className='text-xl text-blue-950 font-bold hover:text-black'>UP Scolorship</Link>
+        </div>
+        <div>
+          <Link className='text-xl text-blue-950 font-bold hover:text-black'>National Scolorship</Link>
+        </div>
+      </div> 
     </div>
   )
 }
