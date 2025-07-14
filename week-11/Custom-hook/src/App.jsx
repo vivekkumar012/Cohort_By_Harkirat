788f8473c45ada9b30f3ae8bc607a9cad810375a
Vivek Kumar <vh118854@gmail.com>
2025-07-15 00:48:21 +0530

    last night

--- week-11/Custom-hook/src/App.jsx
+++ week-11/Custom-hook/src/App.jsx
@@ -1,39 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useFetch } from './hooks/useFetch'
 
 //Custom hook
-function useCounter() {
-  const [count, setCount] = useState(0);
+// function useCounter() {
+//   const [count, setCount] = useState(0);
 
-  function increaseCount() {
-    setCount(count+1);
-  }
-  return{
-    count: count,
-    increaseCount: increaseCount
-  }
-}
+//   function increaseCount() {
+//     setCount(count+1);
+//   }
+//   return{
+//     count: count,
+//     increaseCount: increaseCount
+//   }
+// }
 
-function App() {
+
+
+function App() { 
+  const [currentPost, setCurrentPost] = useState(1);
+  const { finaldata } = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
+
 
   return (
      <div>
+      {/* <Counter />
       <Counter />
       <Counter />
-      <Counter />
-      <Counter />
+      <Counter /> */}
+      <button onClick={() => setCurrentPost(1)}>1</button>
+      <button onClick={() => setCurrentPost(2)}>2</button>
+      <button onClick={() => setCurrentPost(3)}>3</button>
+      {JSON.stringify(finaldata)}
      </div>
   )
 }
 
-function Counter() {
-  const {count, increaseCount} = useCounter()
+// function Counter() {
+//   const {count, increaseCount} = useCounter()
 
-  return (
-     <div>
-      <button onClick={increaseCount}>increase {count}</button>
-     </div>
-  )
-}
+//   return (
+//      <div>
+//       <button onClick={increaseCount}>increase {count}</button>
+//      </div>
+//   )
+// }
 
 export default App
---
+++ week-11/Custom-hook/src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+//custom hook
+export function usePostTitle() {
+    const [post, setPost] = useState({});
+
+    async function getPosts() {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+        const json = await response.json()
+        setPost(json);
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, [])
+
+    return post.title;
+}
+
+export function useFetch(url) {
+    const [finaldata, setFinaldata] = useState({})
+
+    async function getDetails() {
+        const response = await fetch(url)
+        const json = response.json()
+        setFinaldata(json);
+    }
+
+    useEffect(() => {
+        getDetails();
+    }, [url])
+
+    return {
+        finaldata
+    }
+}
