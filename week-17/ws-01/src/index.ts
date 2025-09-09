74b6f01481d920f45e2666d4e66a23e841114bf7
Vivek Kumar <vh118854@gmail.com>
2025-09-09 22:58:51 +0530

    new codes added

--- week-17/ws-01/src/index.ts
+++ week-17/ws-01/src/index.ts
@@ -2,9 +2,12 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (server) => {
-    console.log("Hi Connection")
-    setInterval(() => {
-        server.send("hello ram");
-    }, 5000)
+wss.on("connection", (socket) => {
+    console.log("Server Connected")
+    
+    socket.on("message" , (e) => {
+        if(e.toString() === "ping") {
+            socket.send("pong");
+        }
+    })
 })
