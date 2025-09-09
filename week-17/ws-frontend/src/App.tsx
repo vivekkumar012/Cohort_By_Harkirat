7fb32582bd0f81a300560737b7cd353149276b40
Vivek Kumar <vh118854@gmail.com>
2025-09-09 22:59:34 +0530

    frontend

---
+++ week-17/ws-frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  function sendMsg() {
+
+  }
+
+  return (
+    <div>
+      <input type="text" placeholder="Messages....." />
+      <button onClick={sendMsg}>Send</button>
+    </div>
+  )
+}
+
+export default App
