217f2da7d801afdff128fb4d32d12b146570b1c7
Vivek Kumar <vh118854@gmail.com>
2025-09-30 03:08:34 +0530

    postgres

--- week-18/src/index.ts
+++ week-18/src/index.ts
@@ -1,18 +1,45 @@
 import { Client } from 'pg'
+import express from "express"
 
-// const pgClient = new Client("postgresql://neondb_owner:npg_1FvlXct7niSV@ep-steep-boat-ad3clyih-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+const pgClient = new Client("postgresql://neondb_owner:npg_1FvlXct7niSV@ep-steep-boat-ad3clyih-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
-const pgClient = new Client({
-    user: "neondb_owner",
-    password: "npg_1FvlXct7niSV",
-    port: 5432,
-    host: "ep-lucky-show-a50i"
+// const pgClient = new Client({
+//     user: "neondb_owner",
+//     password: "npg_1FvlXct7niSV",
+//     port: 5432,
+//     host: "ep-lucky-show-a50i"
+// })
+
+pgClient.connect();
+
+const app = express();
+
+app.post("/signup", async (req, res) => {
+    try {
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const city = req.body.city;
+        const country = req.body.country;
+        const street = req.body.street;
+        const pincode = req.body.pincode;
+
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES($1, $2, $3) RETURNING id;`
+        const addressInsertQuery = `INSERT INTO address (city, country, street, pincode, user_id) VALUES($1, $2, $3, $4, $5);`
+
+        await pgClient.query("BEGIN;")
+        const response = await pgClient.query(insertQuery, [username, email, password]);
+        const userId = response.rows[0].id;
+        const addressInsertResponse = await pgClient.query(addressInsertQuery, [city, country, street, pincode, userId]);
+        await pgClient.query("COMMIT;")
+
+        res.json({
+            message: "You are signed up"
+        })
+    } catch (error) {
+        console.log(error);
+    }
 })
 
-async function main() {
-    await pgClient.connect();
-    const response = await pgClient.query("SELECT * FROM users;");
-    console.log(response);
-}
-
-main();
+app.listen(3000);
